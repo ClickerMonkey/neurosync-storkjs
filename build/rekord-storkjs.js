f43968825f9a9eedf347967478613b22db5980d5
Philip Diffenderfer <pdiffenderfer@gmail.com>
2016-10-30 04:18:55 -0400

    Switched to UMD.

--- build/rekord-storkjs.js
+++ build/rekord-storkjs.js
@@ -1,6 +1,29 @@
-/* rekord-storkjs 1.4.2 - A rekord binding to storkjs - implementing Rekord.store by Philip Diffenderfer */
-(function(global, Rekord, Stork, undefined)
+/* rekord-storkjs 1.4.3 - A rekord binding to storkjs - implementing Rekord.store by Philip Diffenderfer */
+// UMD (Universal Module Definition)
+(function (root, factory)
 {
+  if (typeof define === 'function' && define.amd) // jshint ignore:line
+  {
+    // AMD. Register as an anonymous module.
+    define(['Rekord', 'Stork'], function(Rekord, Stork) { // jshint ignore:line
+      return factory(root, Rekord, Stork);
+    });
+  }
+  else if (typeof module === 'object' && module.exports)  // jshint ignore:line
+  {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(global, require('Rekord'), require('Stork'));  // jshint ignore:line
+  }
+  else
+  {
+    // Browser globals (root is window)
+    root.Rekord = factory(root, root.Rekord, root.Stork);
+  }
+}(this, function(global, Rekord, Stork, undefined)
+{
+
   function StoreFactory(database)
   {
     return new Stork( database );
@@ -8,4 +31,6 @@
 
   Rekord.setStore( StoreFactory, true );
 
-})( this, this.Rekord, this.Stork );
+  return Rekord;
+
+}));
--- src/stork.js
+++ src/stork.js
@@ -1,5 +1,28 @@
-(function(global, Rekord, Stork, undefined)
+// UMD (Universal Module Definition)
+(function (root, factory)
 {
+  if (typeof define === 'function' && define.amd) // jshint ignore:line
+  {
+    // AMD. Register as an anonymous module.
+    define(['Rekord', 'Stork'], function(Rekord, Stork) { // jshint ignore:line
+      return factory(root, Rekord, Stork);
+    });
+  }
+  else if (typeof module === 'object' && module.exports)  // jshint ignore:line
+  {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory(global, require('Rekord'), require('Stork'));  // jshint ignore:line
+  }
+  else
+  {
+    // Browser globals (root is window)
+    root.Rekord = factory(root, root.Rekord, root.Stork);
+  }
+}(this, function(global, Rekord, Stork, undefined)
+{
+
   function StoreFactory(database)
   {
     return new Stork( database );
@@ -7,4 +30,6 @@
 
   Rekord.setStore( StoreFactory, true );
 
-})( this, this.Rekord, this.Stork );
+  return Rekord;
+
+}));
